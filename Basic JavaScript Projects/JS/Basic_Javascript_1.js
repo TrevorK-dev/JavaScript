ac2673f87ffcd6568800830c1799e68594fb410f
TrevorK-dev <trevor.knisley01@gmail.com>
2020-02-25 15:29:32 -0800

    initial commit
    new javascript repository, made with GitHub Desktop

---
+++ Basic JavaScript Projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,15 @@
+var A = "This is the document for David Dobriks Vlog"
+document.write(A);
+window.alert(A);
+
+document.write("\" Be who you are and say what you feel,"
++ "Because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+document.write(Family);
+
+var blues = " I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
---
+++ Basic JavaScript Projects/JS/Basic_Javascript_2.js
@@ -0,0 +1,11 @@
+function Function_2() {
+    var str = "Yeet";
+    var result = str.fontcolor("red");
+    document.getElementById("Red_Text").innerHTML = result;
+}
+
+function myFunction() {
+    var sentance = "I am learning";
+    sentance += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentance;
+}
